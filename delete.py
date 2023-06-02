52d8d69c7b39b7ab04aa0fa4dfe86c01de7bed00
Nik Shu <nikoshul1@yandex.ru>
2023-06-02 18:29:13 +0300

    masking values

--- delete.py
+++ delete.py
@@ -6,15 +6,15 @@ context = ssl.SSLContext(ssl.PROTOCOL_TLS)
 context.verify_mode = ssl.CERT_NONE
 
 # Set variables for the IP address and csrf token
-ip_address = "10.2.145.50"
-csrf_token = "OTFmMzA4MzAtN2RlZC00NWY1LWFmYTktZGY1MDExYmU3ZDA1"
+ip_address = "X"
+csrf_token = "X"
 
 conn = http.client.HTTPSConnection(ip_address, 8443, context=context)
 headers = {
   '-csrf-token': csrf_token,
   'X-Requested-With': 'XMLHttpRequest',
   'Content-Type': 'application/json',
-  'Cookie': 'sw_session=616ebb29-a41a-4dae-bf2c-3409b887e99f.hro8gXuKf4mz62drkLUNQodt2Wo15ZMyunCTW35Ys50'
+  'Cookie': 'sw_session=X'
 }
 
 for i in range(1, 101):
